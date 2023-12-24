2efc6e804b6405ae967e3546953fe79101f2794e
Wellington <wellington-158@hotmail.com>
2023-12-24 15:17:45 -0300

    🚧 feat: creating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingsTabs } from '@/components/SettingsTabs.tsx'
+
 export default function Home() {
-	return <h1>Hello</h1>
+	return (
+		<>
+			<h1 className="text-medium text-3xl text-zinc-900">Settings</h1>
+			<SettingsTabs />
+		</>
+	)
 }
