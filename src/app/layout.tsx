d5f925071f4f08c3e4fbd1fcc241ffc775ffeeff
Wellington <wellington-158@hotmail.com>
2023-12-24 15:17:28 -0300

    feat: added antialised style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
-		<html lang="en">
+		<html lang="en" className="antialiased">
 			<body className={inter.className}>
-				<div className="grid-cols-app grid min-h-screen">
+				<div className="grid min-h-screen grid-cols-app">
 					<Sidebar />
 
 					<main className="px-4 pb-12 pt-8">{children}</main>
